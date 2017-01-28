f615ec2bc53188d7dc63c53c5f645c23937a8463
lauryndbrown <lauryndbrown@gmail.com>
2017-01-28 17:39:14 -0600

    Removed ascii_game modules.

--- ascii_game/ascii_game/player.py
+++
@@ -1,8 +0,0 @@
-class Player:
-    """
-    Generic Player Class used by Game
-    """
-    def __init__(self, name, high_score=0):
-        self.name = name
-        self.high_score = high_score
-        self.score = 0
